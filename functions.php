5232287ef5680c926f6b0db1a5365b48895cd189
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-12-03 15:27:08 -0600

    function return array

--- functions.php
+++ functions.php
@@ -22,5 +22,20 @@
 	}
 
 	echo format_name('walter', 'ChaUnte', 'vandeburgh');
+	echo "<br>";
+
+	//first funtion with array
+	function format_name_array($first, $middle, $last)
+	{
+		$first = ucfirst(strtolower($first));
+		$middle = ucfirst(strtolower($middle));
+		$last = ucfirst(strtolower($last));
+
+		return array($first, $middle, $last);
+	}
+
+	$names = format_name_array('wAlter', 'chaunte', 'vandEburgh');
+
+	echo $names[0] . ' ' . $names[1] . ' ' . $names[2];
 
 ?>
