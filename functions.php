29d948dc0db7b4b27c905013cef33a0feee175a9
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-12-03 15:09:21 -0600

    built in functions

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+	//build in functions
+	echo "reversed string: ";
+	echo strrev('hello world');
+	echo "<br>";
+	echo "repeat this string: ";
+	echo str_repeat("hello ", 5);
+	echo "<br>";
+	echo "Transform to uppercase: ";
+	echo strtoupper("hello, world!");
+	echo "<br>";
+
+?>
