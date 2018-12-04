872b606ecf330a9c94c469f716c79edccc71d27f
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2018-12-04 15:51:51 -0600

    array keyword

--- arrays.php
+++ arrays.php
@@ -1,11 +1,36 @@
 <?php 
 
+	//numerical array
 	$paper[] = "Copier";
 	$paper[] = "Inkjet";
 	$paper[] = "Laser";
 	$paper[] = "Photo";
 
+	//for loop with numberical array
 	for ($i = 0; $i < 4; ++$i)
 		echo "$i: $paper[$i]" . "<br>";
+
+	//associative array
+	$printer['copier'] = "Copier & multipur";
+	$printer['inkjet'] = "inkjet printer";
+	$printer['laser'] = "Laser Printer";
+	$printer['photo'] = "Photographic Paper";
+
+	echo $printer['laser'];
+	echo "<br>";
+
+	//array keyword
+	$pages = array('one', 'two', 'three', 'four');
+
+	echo $pages[1];
+	echo "<br>";
+
+	$pages2 = array(
+		'printer' => "lazer",
+		'cat' => "dog"
+	);
+
+	echo $pages2['cat'];
+	echo "<br>";
 
 ?>
